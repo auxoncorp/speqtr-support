7d7c902f68752140827f06ec00dd81fe7915920d
Russell Mull <russell@auxon.io>
2023-03-12 20:38:52 -0700

    Support for 'conform eval' task, used by code lens

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -1,23 +1,82 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as vscode from "vscode";
-// TODO
 import { workspace, ExtensionContext } from 'vscode';
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
-console.log("load");
-
 export function activate(context: ExtensionContext) {
-    console.log("activate");
+    activateLspClient();
 
+    const commandHandler = (args: SpecEvalCommandArgs) => {
+        let commandArgs = [
+            "spec", "eval",
+            "--file", vscode.Uri.parse(args.document_uri).fsPath,
+            "--no-source-context" // Required for the problemMatcher regex to work
+        ];
+
+        if (args.behavior) {
+            commandArgs.push("--behavior", args.behavior);
+        }
+
+        if (args.dry_run) {
+            commandArgs.push("--dry-run")
+        }
+
+        const taskDef: vscode.TaskDefinition = {
+            type: "speqtr.conform",
+            command: "/home/mullr/devel/modality/target/debug/conform",
+            args: commandArgs
+        };
+
+        let problemMatchers = ["$conformEval"];
+        const scope = vscode.workspace.workspaceFolders![0];
+        const exec = new vscode.ProcessExecution(taskDef.command, taskDef.args);
+        const target = vscode.workspace.workspaceFolders![0];
+
+        let task = new vscode.Task(
+            taskDef, scope, "conform", "conform source",
+            exec, problemMatchers);
+
+        task.group = vscode.TaskGroup.Build;
+        task.presentationOptions = {
+            echo: true,
+            reveal: vscode.TaskRevealKind.Always,
+            panel: vscode.TaskPanelKind.Dedicated,
+            clear: true
+        };
+
+        return vscode.tasks.executeTask(task);
+    };
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand("speqtr.conform", commandHandler)
+    );
+}
+
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
+
+type SpecEvalCommandArgs = {
+    document_uri: string,
+    behavior?: string,
+    dry_run: boolean,
+};
+
+
+function activateLspClient() {
     var serverConfig : vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("speqtr.server");
 
     var serverPath = serverConfig.get<null | string>("path");
@@ -29,7 +88,7 @@ export function activate(context: ExtensionContext) {
             if (!fs.existsSync(serverPath)) {
                 serverPath = "/usr/bin/speqtr_lsp";
             }
-        }   
+        }
     }
 
     console.info(`Using lsp executable at ${serverPath}`);
@@ -51,37 +110,30 @@ export function activate(context: ExtensionContext) {
     }
 
     debugEnv['RUST_LOG'] = 'trace';
-	const debugOptions = { env: debugEnv };
+    const debugOptions = { env: debugEnv };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { command: serverPath, transport: TransportKind.ipc, options: { env: env } },
-		debug: {
-			command: serverPath,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    const serverOptions: ServerOptions = {
+        run: { command: serverPath, transport: TransportKind.ipc, options: { env: env } },
+        debug: {
+            command: serverPath,
+            transport: TransportKind.ipc,
+            options: debugOptions
+        }
+    };
 
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'speqtr' }]
-	};
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'speqtr' }]
+    };
 
-	client = new LanguageClient(
-		'speqtrLanguageServer',
-		'SpeQTr Language Server',
-		serverOptions,
-		clientOptions
-	);
+    client = new LanguageClient(
+        'speqtrLanguageServer',
+        'SpeQTr Language Server',
+        serverOptions,
+        clientOptions
+    );
 
-	// Start the client. This will also launch the server
-	client.start();
-}
-
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    // Start the client. This will also launch the server
+    client.start();
 }
