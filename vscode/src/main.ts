3b3e1ec5e388d548ca9685ab15a0ee02ec1bf3f7
Russell Mull <russell@auxon.io>
2023-04-13 08:34:37 -0700

    Linkify coord..coord ranges

--- vscode/src/main.ts
+++ vscode/src/main.ts
@@ -54,31 +54,56 @@ export async function activate(context: ExtensionContext) {
     );
 }
 
+const EVENT_COORDS_RE_STR: string = "%[0-9a-f]{32}:[0-9a-f]+";
+// Look for '..' ahead and behind, so we don't  match part of a coord..coord range
+const EVENT_COORDS_RE: RegExp = RegExp(`(?<!\\.\\.)(${EVENT_COORDS_RE_STR})(\\.\\.)?`, "g");
+const EVENT_COORDS_RANGE_RE: RegExp = RegExp(`(${EVENT_COORDS_RE_STR})\\.\\.(${EVENT_COORDS_RE_STR})`, "g");
+
 const EVENT_COORDS_TERMINAL_LINK_PROVIDER: vscode.TerminalLinkProvider = {
     provideTerminalLinks: (context: vscode.TerminalLinkContext, token: vscode.CancellationToken) => {
         var links = [];
-        const event_coords_re = RegExp("%[0-9a-f]{32}:[0-9a-f]+", "g");
 
-        let exec_res_array = event_coords_re.exec(context.line);
-        if (exec_res_array != null) {
-            for (const exec_res of exec_res_array) {
-                links.push({
-                    startIndex: exec_res_array.index,
-                    length: exec_res.length,
-                    tooltip: 'View log around this event',
-                    data: exec_res
-                });
-
+        for (const match of context.line.matchAll(EVENT_COORDS_RE)) {
+            if (match[2] == "..") {
+                continue;
             }
+
+            const coord = match[2];
+            links.push({
+                startIndex: match.index,
+                length: match[0].length,
+                tooltip: 'View log around this event',
+                data: { around: coord, radius: 5 }
+            });
+        }
+
+        for (const match of context.line.matchAll(EVENT_COORDS_RANGE_RE)) {
+            const firstCoord = match[1];
+            const secondCoord = match[2];
+
+            links.push({
+                startIndex: match.index,
+                length: match[0].length,
+                tooltip: 'View log of this causal region',
+                data: { from: firstCoord, to: secondCoord }
+            });
         }
 
         return links;
     },
     handleTerminalLink: (link: any) => {
-        vscode.commands.executeCommand(MODALITY_LOG_COMMAND, { type: "LogCommandArgs", around: link.data, radius: 5 });
+        vscode.commands.executeCommand(
+            MODALITY_LOG_COMMAND,
+            {
+                type: "LogCommandArgs",
+                around: link.data.around,
+                radius: link.data.radius,
+                from: link.data.from,
+                to: link.data.to,
+            }
+        );
     }
 };
-
 
 export function deactivate(): Thenable<void> | undefined {
     if (!lspClient) {
