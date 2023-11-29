1599de5f980facdc6901ada237f103410f972df5
Russell Mull <russell@auxon.io>
2023-11-29 09:35:25 -0800

    Fix failure accounting in coverage report

--- vscode/src/specCoverage.ts
+++ vscode/src/specCoverage.ts
@@ -191,13 +191,10 @@ function behaviorViewModel(bhCov: api.BehaviorCoverage): BehaviorViewModel {
         .reduce((a, b) => a + b);
 
     let status: Status = "not-executed";
-    // TODO use api value once it exists
-    if (bhCov.test_counts.ever_executed && triggerCount > 0) {
-        if (bhCov.test_counts.ever_failed) {
-            status = "failed";
-        } else {
-            status = "passed";
-        }
+    if (bhCov.test_counts.ever_failed) {
+        status = "failed";
+    } else if (bhCov.test_counts.ever_executed && triggerCount > 0) {
+        status = "passed";
     }
 
     return {
