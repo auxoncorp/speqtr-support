7fe62654f2ba99d570e8421df0a8c5eee8f609e7
Jon Lamb <jon@auxon.io>
2024-03-07 07:35:21 -0800

    Add event inspect to speqtr links, allow for whitespace in event-at-timeline regex

--- vscode/src/modalityEventInspect.ts
+++ vscode/src/modalityEventInspect.ts
@@ -15,6 +15,10 @@ function runModalityEventInspectCommand(event: string) {
     // We're going to send the text of the command line to the terminal. Build up the args list here.
     const modalityArgs = ["LESS=R", modality, "event", "inspect", event];
 
+    for (const extra of config.extraCliArgs("modality event inspect")) {
+        modalityArgs.push(extra);
+    }
+
     const term: vscode.Terminal = vscode.window.createTerminal({
         name: "modality event inspect",
         location: vscode.TerminalLocation.Editor,
