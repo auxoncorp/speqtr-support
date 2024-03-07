f086bd344c2d70013107630e5157dc6259667bbb
Russell Mull <russell@auxon.io>
2024-03-07 07:59:00 -0800

    Fix build script

--- vscode/scripts/copyResources.js
+++ vscode/scripts/copyResources.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 
+if (!fs.existsSync("resources/dist")) {
+    fs.mkdirSync("resources/dist");
+}
+
 fs.copyFile("node_modules/@vscode/codicons/dist/codicon.css", "resources/dist/codicon.css", (err) => {
     if (err) throw err;
     console.log("codicon.css was copied to resources/dist");
